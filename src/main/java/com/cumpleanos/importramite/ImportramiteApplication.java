e1a5986ad6f91c7b8f9bb0bf34488c0eb3baae8b
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-17 10:46:16 -0500

    Project Initialization

---
+++ src/main/java/com/cumpleanos/importramite/ImportramiteApplication.java
@@ -0,0 +1,13 @@
+package com.cumpleanos.importramite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportramiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportramiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cumpleanos/importramite/ImportramiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cumpleanos.importramite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportramiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
