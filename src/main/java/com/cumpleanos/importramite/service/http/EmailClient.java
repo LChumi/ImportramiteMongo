80d0677a51e3ef606d018e915e98c4f42569cba9
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-03-13 12:00:07 -0500

    feat:connect to MongoDB and changes in feignClient

--- src/main/java/com/cumpleanos/importramite/service/http/EmailClient.java
+++ src/main/java/com/cumpleanos/importramite/service/http/EmailClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name = "notification-service", url = "http://localhost:8083/email")
+@FeignClient(name = "notification-service", url = "http://192.168.112.245:8083/email")
 public interface EmailClient {
 
     @PostMapping(value = "/enviar/adjunto", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
