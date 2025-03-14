d0c04b2232137784aee0f8bc1ff4226be06ef39a
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-03-14 16:20:11 -0500

    feat: added corsOrigin

--- src/main/java/com/cumpleanos/importramite/presentation/controller/EmailController.java
+++ src/main/java/com/cumpleanos/importramite/presentation/controller/EmailController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("email")
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class EmailController {
