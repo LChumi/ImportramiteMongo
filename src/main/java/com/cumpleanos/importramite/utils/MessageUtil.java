4912f6c9224982cf437977165e090eee5a927935
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-04-25 12:54:23 -0500

    style: chnages in format message to send mail

--- src/main/java/com/cumpleanos/importramite/utils/MessageUtil.java
+++ src/main/java/com/cumpleanos/importramite/utils/MessageUtil.java
@@ -4,7 +4,7 @@ public class MessageUtil {
 
     public static String MENSAJE_TRAMITE(String tramite, String fecha, String contenedores) {
         return String.format("""
-                        <div style='font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px;'>
+                        <div style='font-family: Arial, sans-serif; background-color: #354edc; padding: 20px;'>
                                 <div style='background-color: #ffffff; border-radius: 10px; padding: 30px; max-width: 600px; margin: auto; box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);'>
                                     <h2 style='color: #333;'>Notificación de Trámite</h2>
                                     <p style='font-size: 16px; color: #555;'>
