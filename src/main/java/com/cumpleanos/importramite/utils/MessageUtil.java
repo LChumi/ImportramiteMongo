38d3de83dc5ca94a7b81daaaa4838b0f0e6a68f4
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-04-28 16:10:05 -0500

    refactor: changes in styles in message Utils

--- src/main/java/com/cumpleanos/importramite/utils/MessageUtil.java
+++ src/main/java/com/cumpleanos/importramite/utils/MessageUtil.java
@@ -4,7 +4,6 @@ public class MessageUtil {
 
     public static String MENSAJE_TRAMITE(String tramite, String fecha, String contenedores) {
         return String.format("""
-                        <div style='font-family: Arial, sans-serif; background-color: #354edc; padding: 20px;'>
                                 <div style='background-color: #ffffff; border-radius: 10px; padding: 30px; max-width: 600px; margin: auto; box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);'>
                                     <h2 style='color: #333;'>Notificación de Trámite</h2>
                                     <p style='font-size: 16px; color: #555;'>
@@ -21,7 +20,6 @@ public class MessageUtil {
                                         Saludos cordiales.
                                     </p>
                                 </div>
-                        </div>
                 """, tramite, fecha, contenedores);
     }
 }
