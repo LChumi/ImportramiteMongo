fb327fd930e520d45eeeead6c0b24c3d7d8bb49d
LChumi <luischumi.9@gmail.com>
2026-05-06 11:14:51 -0500

    feat: added new services into usuario_bod and update controllers

--- src/main/java/com/cumpleanos/importramite/presentation/controller/UsuarioController.java
+++ src/main/java/com/cumpleanos/importramite/presentation/controller/UsuarioController.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
-@RequestMapping("usuario")
+@RequestMapping("usuario_bod")
 @RequiredArgsConstructor
 public class UsuarioController {
 
