d92f323c62b1068667c17d8a3a9d3fc94acd7953
LChumi <luischumi.9@gmail.com>
2026-05-06 12:08:55 -0500

    fix: changes into repository to get menus by List added queryt anotation

--- src/main/java/com/cumpleanos/importramite/persistence/repository/MenuRepository.java
+++ src/main/java/com/cumpleanos/importramite/persistence/repository/MenuRepository.java
@@ -2,10 +2,13 @@ package com.cumpleanos.importramite.persistence.repository;
 
 import com.cumpleanos.importramite.persistence.model.Menu;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface MenuRepository extends MongoRepository<Menu, String> {
 
-    List<Menu> findByRolesPermitidos(List<String> rolesPermitidos);
+    @Query("{ 'rolesPermitidos': { $in: ?0 } }")
+    List<Menu> findByRolesPermitidos(List<String> roles);
 }
