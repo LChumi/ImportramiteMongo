1293a691893c6266705b434329cbf9898ab876b7
LChumi <luischumi.9@gmail.com>
2026-05-07 18:30:48 -0500

    styles: changes into styles and update component

--- src/main/java/com/cumpleanos/importramite/presentation/controller/ProductoObservacionController.java
+++ src/main/java/com/cumpleanos/importramite/presentation/controller/ProductoObservacionController.java
@@ -12,29 +12,30 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
-@RequestMapping("mongo")
+@RequestMapping("observacion")
 @RequiredArgsConstructor
-@Tag(name = "Producto Observacio", description = "Documentacion API Producto Correos")
+@Tag(name = "Producto Observacio", description = "Documentacion API Observaciones de productos")
 public class ProductoObservacionController {
 
     private final IProductoObservacionService service;
 
     @Operation(summary = "Guardar", description = "Guardar Observacion de Producto")
-    @PostMapping("/observacion/guardar")
+    @PostMapping("/guardar")
     public ResponseEntity<ProductoObservacion> saveProductoObservacion(@RequestBody ProductoObservacion productoObservacion) {
         return ResponseEntity.ok(service.saveObservation(productoObservacion));
     }
 
     @Operation(summary = "Listar", description = "Listar Observaciones de Productos")
     @Parameter(name = "bodegaId", description = "Id de la bodega donde se encuentra la observacion del producto")
-    @GetMapping("/observacion/listar/{bodegaId}")
+    @GetMapping("/listar/{bodegaId}")
     public ResponseEntity<List<ProductoObservacion>> listarProductoObservacion(@PathVariable Long bodegaId) {
         return ResponseEntity.ok(service.findByBodega(bodegaId));
     }
 
     @Operation(summary = "Correccion", description = "Agregar correccion de productos")
-    @PutMapping("/observacion/agregar-correccion")
+    @PutMapping("/agregar-correccion")
     public ResponseEntity<ProductoObservacion> agregarCorreccion(@RequestBody CorreccionRequest request) {
         return ResponseEntity.ok(service.addCorrection(request));
     }
